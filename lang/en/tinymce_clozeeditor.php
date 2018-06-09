9c810e765cc9acea52155a2f670063fa0bc0f90d
Germán Valero <gvalero@unam.mx>
2018-06-09 11:26:55 -0500

    Update English language strings

--- lang/en/tinymce_clozeeditor.php
+++ lang/en/tinymce_clozeeditor.php
@@ -1,5 +1,4 @@
 <?php
-
 // This file is part of Moodle - http://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
@@ -14,20 +13,17 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
-
 /**
  *
  *
  * @package    tiny_mce
  * @subpackage clozeeditor
  * @copyright  2013 Andreas Glombitza/Achim Skuta
+ * @copyright  2018 onwards Germán Valero (gvalero@unam.mx)
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-
-
 $string['pluginname'] = 'Cloze Editor';
 $string['privacy:metadata'] = 'The TinyMCE Cloze editor plugin does not store any personal data.';
-
 $string['clozeeditor:desc'] = 'This is an editor for CLOZE questions';
 $string['clozeeditor:chooseclozeformat'] = 'Please choose a Cloze sub-question format:';
 $string['clozeeditor:encode'] = 'Encode';

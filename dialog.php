c4c91f1b37c3837bbcbdb01c244bc46beaddb71e
Germán Valero <gvalero@unam.mx>
2018-06-09 11:38:48 -0500

    Whitespace found at end of line

--- dialog.php
+++ dialog.php
@@ -1,16 +1,26 @@
-<?php // $Id: insert_cloze.php,v 1.4 2013/18/03
-
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+// $Id: insert_cloze.php,v 1.4 2013/18/03
 define('NO_MOODLE_COOKIES', true); // Session not used here.
-
 require(dirname(dirname(dirname(dirname(dirname(dirname(__FILE__)))))) . '/config.php');
-
 $PAGE->set_context(context_system::instance());
 $PAGE->set_url('/lib/editor/tinymce/plugins/clozeeditor/dialog.php');
 $stringmanager = get_string_manager();
-
 $editor = get_texteditor('tinymce');
 $plugin = $editor->get_plugin('clozeeditor');
-
 $htmllang = get_html_lang();
 header('Content-Type: text/html; charset=utf-8');
 header('X-UA-Compatible: IE=edge');
@@ -74,11 +84,10 @@ header('X-UA-Compatible: IE=edge');
 </form>
 
 <form onsubmit="clozeeditorDialog.insert();return false;" action="#">
-
-	<div class="mceActionPanel">
-		<input type="button" id="insert" name="insert" value="{#insert}" onclick="clozeeditorDialog.insert();" />
-		<input type="button" id="cancel" name="cancel" value="{#cancel}" onclick="tinyMCEPopup.close();" />
-	</div>
+        <div class="mceActionPanel">
+                <input type="button" id="insert" name="insert" value="{#insert}" onclick="clozeeditorDialog.insert();" />
+                <input type="button" id="cancel" name="cancel" value="{#cancel}" onclick="tinyMCEPopup.close();" />
+        </div>
 </form>
 
 </body>

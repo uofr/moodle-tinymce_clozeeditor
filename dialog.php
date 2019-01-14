2268967559a033f2d46d60bdbc0055d23a13d231
Germán Valero <gvalero@unam.mx>
2019-01-13 18:24:19 -0600

    Inline comments must end in full-stops

--- dialog.php
+++ dialog.php
@@ -13,7 +13,7 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
-// $Id: insert_cloze.php,v 1.4 2013/18/03
+// $Id: insert_cloze.php,v 1.4 2013/18/03.
 
 
 /**
